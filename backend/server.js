abeecc4d8eb28f5c04d4c5f723967ea665de85b9
FahidShadmanKarim <fahid.shadman35@gmail.com>
2024-11-02 17:49:29 +0600

    Changed the Redis URL

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/users',userRoutes);
 
 socketHandler(server);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
